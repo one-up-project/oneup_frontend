021317d04bdc1e99bade59f519cf9ef257d3efec
Gustavo Rivera <griverab@unal.edu.co>
2024-12-18 23:42:23 -0500

    implement request to search_ms for store locations

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -1,27 +1,49 @@
-import React, { useState } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import React, { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { UserLocationIcon } from "./UserLocationIcon";
 import "leaflet/dist/leaflet.css";
 import Markers from "./Markers";
-import { coordinates } from "../assets/data";
 
 const MapView = () => {
+  //ubicacion del usuario actual
   const [state, setState] = useState({
-    concurentLocation: {
+    currentLocation: {
       lat: 4.63634,
       lng: -74.083199,
     },
     zoom: 14,
   });
 
+  const [data, setData] = useState([]);
+  // peticion servicio search para busqueda de tiendas en un radio de 2km
+  useEffect(() => {
+    fetch(
+      `http://localhost:8800/search-ms/stores/get-near-stores/${state.currentLocation.lat}/${state.currentLocation.lng}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data); // Actualizar el estado con los datos
+      })
+      .catch((error) => {
+        console.error("Error al hacer la petición:", error);
+      });
+  }, []);
+
   return (
     <div>
       <h2>Tiendas cercanas a ti</h2>
-      <MapContainer center={state.concurentLocation} zoom={state.zoom}>
+      <MapContainer center={state.currentLocation} zoom={state.zoom}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Markers places={coordinates} />
+        {/* marcadores segun tiendas obtenidas */}
+        <Markers places={data} />
+        {/* marcador posicion usuario actual */}
+        <Marker
+          position={[state.currentLocation.lat, state.currentLocation.lng]}
+          icon={UserLocationIcon}
+        />
       </MapContainer>
     </div>
   );
--- frontend/src/components/Markers.jsx
+++ frontend/src/components/Markers.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { LocationIcon } from "./LocationIcon";
 import { Marker } from "react-leaflet";
 
 const Markers = (props) => {
   const { places } = props;
   const markers = places.map((place, i) => (
-    <Marker key={i} position={place.geometry} icon={place.icon} />
+    <Marker key={i} position={[place.lat, place.lon]} icon={LocationIcon} />
   ));
 
-    return markers;
+  return markers;
 };
 
 export default Markers;
