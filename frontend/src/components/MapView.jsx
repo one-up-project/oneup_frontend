d16a7e3d6c90330ce8470024c468819fd8613133
Gustavo Rivera <griverab@unal.edu.co>
2024-12-18 19:37:34 -0500

    title added

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -1,27 +1,29 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import Markers from "./Markers";
-import {coordinates} from "../assets/data";
+import { coordinates } from "../assets/data";
 
 const MapView = () => {
-
   const [state, setState] = useState({
     concurentLocation: {
-      lat: 4.636340,
+      lat: 4.63634,
       lng: -74.083199,
     },
     zoom: 14,
   });
 
   return (
-    <MapContainer center={state.concurentLocation} zoom={state.zoom}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Markers places={coordinates} />
-    </MapContainer>
+    <div>
+      <h2>Tiendas cercanas a ti</h2>
+      <MapContainer center={state.concurentLocation} zoom={state.zoom}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Markers places={coordinates} />
+      </MapContainer>
+    </div>
   );
 };
 
