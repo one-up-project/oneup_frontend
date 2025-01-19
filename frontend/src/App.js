189dad49dc4a47fdaec0db43c61ec468a03b3b3f
Gustavo Rivera <griverab@unal.edu.co>
2025-01-18 22:18:28 -0500

    homepage, principalpage, searchpage, routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 import MapView from "./components/MapView";
+import Home from "./pages/Home";
+import Principal_Page from "./pages/Principal_Page";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-  return <MapView />;
+  return (
+    // to do
+    // ver video para implementar rutas
+    <Router>
+      <Routes>
+        <Route exact path="/" component={Principal_Page} /> {/* ruta raiz */}
+        <Route exact path="/home" component={Home} /> {/* ruta home */}
+        <Route exact path="/search" component={MapView} /> {/* ruta search */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Principal_Page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Principal_Page = () => {
+  return (
+    <div>
+      <h1>Welcome to the Principal Page</h1>
+      <p>This is the main content of the page.</p>
+    </div>
+  );
+};
+
+export default Principal_Page;
