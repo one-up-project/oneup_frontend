721e2e38c9af1e3f96474e85f9ad35eede1903f3
Gustavo Rivera <griverab@unal.edu.co>
2025-01-19 21:35:45 -0500

    navbar, homepage, favicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
-import "./App.css";
+import "./layout.scss";
 import MapView from "./components/MapView";
-import Home from "./pages/Home";
-import Principal_Page from "./pages/Principal_Page";
+import Home from "./pages/home/Home";
+import Navbar from "./components/navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    // to do
-    // ver video para implementar rutas
-    <Router>
-      <Routes>
-        <Route exact path="/" component={Principal_Page} /> {/* ruta raiz */}
-        <Route exact path="/home" component={Home} /> {/* ruta home */}
-        <Route exact path="/search" component={MapView} /> {/* ruta search */}
-      </Routes>
-    </Router>
+    <div className="layout">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <Home />
+      </div>
+    </div>
   );
 }
 
