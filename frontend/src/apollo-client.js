8acac6bd7e50db02d63085d41e98598158e92575
Gustavo Rivera <griverab@unal.edu.co>
2025-02-23 23:34:40 -0500

    config var entorno

--- frontend/src/apollo-client.js
+++ frontend/src/apollo-client.js
@@ -1,9 +1,10 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
+const URL_GATEWAY = process.env.REACT_APP_URL_GATEWAY;
 // Apollo Client
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:4000/", 
+    uri: URL_GATEWAY,
   }),
   cache: new InMemoryCache(),
 });
