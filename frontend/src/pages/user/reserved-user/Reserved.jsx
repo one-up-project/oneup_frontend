fd8a72f92e781b9101ea03591e732b1aaaf44202
Gustavo Rivera <griverab@unal.edu.co>
2025-02-23 22:48:32 -0500

    integracion mercadopago

--- frontend/src/pages/user/reserved-user/Reserved.jsx
+++ frontend/src/pages/user/reserved-user/Reserved.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
 import "./reserved.scss";
 import { GET_USER_ORDERS } from "../../../graphql/queries";
-
 import { useQuery } from "@apollo/client";
+import axios from "axios";
+import { useState } from "react";
 
 const Reserved = () => {
+  const [preferenceIds, setPreferenceIds] = useState({});
   //obtener el id del usuario
   const id = JSON.parse(localStorage.getItem("user")).id;
 
@@ -20,9 +23,41 @@ const Reserved = () => {
   } else {
     console.log("ordenes encontradas,", data);
   }
+
+  //integracion mercado pago
+
+  initMercadoPago(process.env.REACT_APP_PUBLIC_KEY, {
+    locale: "es-CO",
+  });
+
+  const createPreference = async (title, price) => {
+    console.log("datos pago:", title, price);
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_PAYMENT_MS}/payments-ms/create_preference`,
+        {
+          title: title,
+          price: price,
+          quantity: 1,
+        }
+      );
+      const { id } = response.data;
+      return id;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handlePayment = async (orderId, title, price) => {
+    const id = await createPreference(title, price);
+    setPreferenceIds((prev) => ({ ...prev, [orderId]: id }));
+  };
+
   return (
-    <div className="layer">
-      <h1>Aqui se listan tus productos reservados</h1>
+    <div className="table-container">
+      <div className="title">
+        <h3>Aqui se listan tus productos reservados</h3>
+      </div>
       <table className="reserved-table">
         <thead>
           <tr>
@@ -35,6 +70,7 @@ const Reserved = () => {
             <th className="reserved-table-header">Estado</th>
             <th className="reserved-table-header">Cancelar</th>
             <th className="reserved-table-header">Pagar</th>
+            <th className="reserved-table-header">Checkout</th>
           </tr>
         </thead>
         <tbody>
@@ -61,7 +97,29 @@ const Reserved = () => {
                   <button className="cancel-button">Cancelar</button>
                 </td>
                 <td className="reserved-table-cell">
-                  <button className="pay-button">Pagar</button>
+                  <button
+                    className="pay-button"
+                    onClick={() =>
+                      handlePayment(
+                        order.order_id,
+                        order.randomBag.description,
+                        order.randomBag.discount_price
+                      )
+                    }
+                  >
+                    Pagar
+                  </button>
+                </td>
+                <td>
+                  {preferenceIds[order.order_id] && (
+                    <div className="wallet">
+                      <Wallet
+                        initialization={{
+                          preferenceId: preferenceIds[order.order_id],
+                        }}
+                      />
+                    </div>
+                  )}
                 </td>
               </tr>
             ))}
