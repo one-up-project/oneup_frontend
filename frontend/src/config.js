dc69ecea57ddb4dcebeb9232358cc90b63513c5e
AlarconCruz <lalarcon@unal.edu.co>
2025-03-02 16:30:08 -0500

    Se cambio las URLs del Apollo server para que se pueda conectar a los servicios en la nube

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
-//export const API_URL = import.meta.env.VITE_API_URL|| "http://localhost:4000/";
-export const API_URL = process.env.APIGATEWAY_URL || "http://localhost:4000/"; // url del api gateway
+// export const API_URL = import.meta.env.VITE_API_URL|| "http://localhost:4000/";
+// export const API_URL = process.env.APIGATEWAY_URL || "http://localhost:4000/"; // url del api gateway
+export const API_URL = process.env.APIGATEWAY_URL || "https://oneup-ag-500667615440.us-central1.run.app"; // url del api gateway
